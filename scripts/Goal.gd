86f62c6e1fea3dfe1e4f14d894d6e1ea0ffc6af0
djalexkidd <alexpyr@outlook.fr>
2021-07-06 22:54:04 +0200

    Ajout d'animations et d'un bouton pour quitter

--- scripts/Goal.gd
+++ scripts/Goal.gd
@@ -4,5 +4,8 @@ func _ready():
 	$TimeValue.text = "%0.3f" % Global.lasttime
 	$HighScoreValue.text = "%0.3f" % Global.highscore
 
-func _on_Button_pressed():
+func _on_RestartButton_pressed():
 	get_tree().change_scene("res://scenes/Game.tscn")
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
