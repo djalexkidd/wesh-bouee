c495c3e92c941b81f1640f5340123d6977dc1a29
djalexkidd <alexpyr@outlook.fr>
2021-07-08 18:52:55 +0200

    Affiche un message si nouveau record

--- scripts/Goal.gd
+++ scripts/Goal.gd
@@ -3,6 +3,8 @@ extends Control
 func _ready():
 	$TimeValue.text = "%0.3f" % Global.lasttime
 	$HighScoreValue.text = "%0.3f" % Global.highscore
+	if Global.lasttime == Global.highscore:
+		$NewHighScore.show()
 
 func _on_RestartButton_pressed():
 	Global.replay()
